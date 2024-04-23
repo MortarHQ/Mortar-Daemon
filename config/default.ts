975a4fc074d1f0e03726ac41f85cc5cb0f31f79b
ZOHI666 <142140319+ZOHI666@users.noreply.github.com>
2024-04-23 13:34:20 +0800

    Update default.ts
    添加注释

--- config/default.ts
+++ config/default.ts
@@ -1,18 +1,18 @@
 const config = {
-  port: process.env.PORT || "8080",
-  host: process.env.HOST || "localhost",
+  port: process.env.PORT || "8080",  //网站偏移管理界面端口
+  host: process.env.HOST || "localhost",  //网站管理界面地址
   logLevel: process.env.LOG_LEVEL || "info",
   logFormat: process.env.LOG_FORMAT || "combined",
-  serverPort: process.env.SERVER_PORT || "25565",
+  serverPort: process.env.SERVER_PORT || "25565",  //伪造的服务器访问端口
 };
 
 if (process.env.NODE_ENV === "development") {
   config.host = "0.0.0.0";
   config.logLevel = "debug";
   config.logFormat = "dev";
-  config.serverPort = "25555";
+  config.serverPort = "25555";  //伪造的服务器访问端口
 } else if (process.env.NODE_ENV !== "production") {
-  config.serverPort = "25555";
+  config.serverPort = "25555";  //伪造的服务器访问端口
 }
 
 export default config;
